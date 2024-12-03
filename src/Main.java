42eb42f53d12831df8f398214c26f316dad200e1
Maria_Prokhorova <luchiksolnza@gmail.com>
2024-12-03 21:44:08 +0700

    commit 3

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.sql.SQLOutput;
 import java.util.*;
 
 public class Main {
@@ -32,16 +33,32 @@ public class Main {
          */
         System.out.println("Задача 3");
         List<String> words = new ArrayList<>(List.of("один", "два", "два", "три", "три", "три"));
+        // 1 вариант решения задачи
         List<String> newWords = removeDuplicates(words);
+        System.out.println("1 вариант решения задачи");
         System.out.println(newWords);
+        // 2 вариант решения задачи, если не важен порядок элементов
+        Set<String> newWords2 = new HashSet<>(words);
+        System.out.println("2 вариант решения задачи");
+        System.out.println(newWords2);
 
         /* Задание 4
         Напишите код, который выводит в консоль количество дублей для каждого уникального слова. Код
         должен работать с любой последовательностью и объемом списка слов.
          */
         System.out.println("Задача 4");
+        //1 вариант решение задачи
         Map<String, Integer> mapCount = countByClassicalLoop(words);
         System.out.println(mapCount);
+        // 2 вариант решение задачи
+        List<String> strings = new ArrayList<>(List.of("один", "два", "два", "три", "три", "три"));
+        Map<String, Integer> wordCounts = new HashMap<>();
+        for (String word : strings) {
+            wordCounts.put(word, wordCounts.getOrDefault(word, 0) + 1);
+        }
+        for (int count : wordCounts.values()) {
+            System.out.println(count);
+        }
 
 
     }
